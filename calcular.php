4e9bb691f2231cb3108f1f4c6c051bf8ac2e56f4
Gustavo Ornaghi Antunes <gustavoornaghiantunes@gmail.com>
2021-07-28 13:41:48 -0300

    alteração do nome da função

--- calcular.php
+++ calcular.php
@@ -1,11 +1,11 @@
 <?php
-function calcular($ano)
+function SeculoAno($ano)
 {
     $seculo = ceil($ano / 100);
     return $seculo;
 }
 $ano = $_POST["ano"];
-echo "seculo: ".calcular($ano);
+echo "seculo: ".SeculoAno($ano);
 ?>
 <br>
 <a href="index.php">Voltar</a>
